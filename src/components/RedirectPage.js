cd39ddbd8325ec65ea1cf753f542b81022fe360d
Juan Tellez <juan.tellez@aa.com>
2021-08-09 09:41:08 -0500

    Handle redirect page after granting access

--- src/components/RedirectPage.js
+++ src/components/RedirectPage.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import _ from 'lodash';
+import { getQueryParams } from '../utils/utils';
 
-const RedirectPage = () => {
-    return <div> Redirect Page</div>;
-};
-export default RedirectPage;
+export default class RedirectPage extends React.Component {
+    componentDidMount() {
+        const { setExpiryTime, history, location } = this.props;
+        try {
+            if (_.isEmpty(location.hash)) {
+                return history.push('/dashboard');
+            }
+            const access_token = getQueryParams(location.hash);
+            const expiryTime = new Date().getTime() + access_token.expires_in * 1000;
+            localStorage.setItem('params', JSON.stringify(access_token));
+            localStorage.setItem('expiry_time',expiryTime);
+            history.push('/dashboard');
+        }
+        catch (err) {
+            history.push('/');
+        }
+    }
+    render() {
+        return null;
+    }
+}
